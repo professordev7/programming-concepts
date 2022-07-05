87cd9bafe640ca331cd81bd9e93fb892edd48c61
Professordev7 <professor.dev.run@gmail.com>
2022-07-05 21:31:10 +0430

    initial commit

---
+++ ex-calculate-median/README.md
@@ -0,0 +1,34 @@
+# Main Goal
+
+It's just a simple javaScript interview question.
+We have a stream of data which stored as an array. We wanna calculate the median of the data.
+-- NOTE: Keep in mind that first of all we need to sort them then calculate.
+
+* CHALLENGES: Sorting data, Calculate median.
+
+* Test Cases:
+
+`(1)
+1, 2, 3, 5, 6, 7, 9 is 5
+Input:
+1
+2
+3
+
+Output:
+1
+1.5
+2
+`
+
+`(2)
+Input 4 5 15 1 3 Expected Output 4 4.5 5 4.5 4`
+
+
+## Available Commands
+
+`$ node task-way1.js`
+
+
+## Useful resources:
+### [JavaScript Array.sort()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
---
+++ ex-calculate-median/way-1.js
@@ -0,0 +1,31 @@
+function printMedian(nums) {
+	// SORTING
+	nums.sort((a, b) => a - b); // *** Perfect use case for sorting ****
+
+	const length = nums.length;
+
+	// Way#1-1 Traditional If condition
+	// 	// NOTE: -1 has been added for proper indexing of the Array.
+	// 	// ODD
+	// 	if (length % 2 === 1) {
+	// 		console.log(nums[(0 + length + 1) / 2 - 1]);
+	// 	}
+	// 	// EVEN
+	// 	if (length % 2 === 0) {
+	// 		console.log((nums[length / 2 - 1] + nums[length / 2 + 1 - 1]) / 2);
+	// 	}
+
+	// Way#1-2 Ternary Operator (More readable)
+	length % 2 === 1
+		? console.log(nums[(0 + length + 1) / 2 - 1])
+		: console.log((nums[length / 2 - 1] + nums[length / 2 + 1 - 1]) / 2);
+}
+
+// Test Cases / Results:
+printMedian([4, 5, 15, 1, 3]);
+
+// printMedian([4]);                // => 4
+// printMedian([4, 5]);             // => 4.5
+// printMedian([4, 5, 15]);         // => 5
+// printMedian([4, 5, 15, 1]);      // SORT: [1, 4, 5, 15] => 4.5
+// printMedian([4, 5, 15, 1, 3]);   // SORT: [1, 3, 4, 5, 15] => 4
